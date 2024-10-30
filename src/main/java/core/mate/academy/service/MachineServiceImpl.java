284bc5bbaefa3701f97b158a66495bd685f756b7
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-30 09:28:23 +0100

    Added MachineServiceImpl class that implements MachineService interface with getAll, fill, startWorking methods

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,42 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    @Override
+    public List<T> getAll(Class<? extends T> type) {
+        if (type == Truck.class) {
+            return (List<T>) new TruckProducer().get();
+        }
+        if (type == Bulldozer.class) {
+            return (List<T>) new BulldozerProducer().get();
+        }
+        if (type == Excavator.class) {
+            return (List<T>) new ExcavatorProducer().get();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (T machine : machines) {
+            machine.doWork();
+        }
+    }
 }
